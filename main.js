e6ad8fe2561f7ba7e7da1139a087df32f6b8c266
Simonedambra <simonedambra130102@gmail.com>
2024-04-24 17:08:24 +0200

    upd sconti nellle varie fasce di età

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 'use strict'
 
 const chilometri = Number(prompt('inserisci km'));
+
 const eta = Number(prompt('inserisci l\'età'));
+
 const prezzokm = Number(0.21)
-let prezzo = Number(chilometri * prezzokm)
 
+const prezzo = Number(chilometri * prezzokm)
 
-console.log(prezzo);
+const percentuale20 = (prezzo / 100 * 80)
+const percentuale40 = (prezzo / 100 * 60)
+
+if (eta < 18) {
+    const prezzo20 = (percentuale20)
+    console.log(prezzo20);
+}
+else if (eta > 65) {
+    const prezzo40 = (percentuale40)
+    console.log(prezzo40);
+}
+
