4d25d60051db995e60e5037df667267d852f5f34
Simonedambra <simonedambra130102@gmail.com>
2024-04-24 15:38:35 +0200

    upd prezzo per km

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const chilometri = Number(prompt('inserisci km'));
+const eta = Number(prompt('inserisci l\'età'));
+const prezzokm = Number(0.21)
+let prezzo = Number(chilometri * prezzokm)
+
+
+console.log(prezzo);
